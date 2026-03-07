643eb3a8fc8c4ef5f00e988dd14981c6b536bc5b
Tejashwa Singh Sengar <tejassengar93@gmail.com>
2026-03-07 14:23:35 +0530

    init commit

---
+++ src/literals.java
@@ -0,0 +1,24 @@
+package com.company;
+public class literals {
+    public static void main(String[]args){
+        int age = 22;
+        byte age2 = 23;
+        long ageDino = 244444444444444L;
+        short age3 = 54;
+        boolean bool = true;
+        boolean bool2 = false;
+        char ch = 'B';
+        float f1 = 6.98f;
+        float f4 = 67543.87f;
+        double d = 598.54;
+        System.out.println(age);
+        System.out.println(age2);
+        System.out.println(age2);
+        System.out.println(age3);System.out.println(ageDino);System.out.println(bool);System.out.println(bool2);System.out.println(ch);
+        System.out.println(f1);System.out.println(f4);System.out.println(d);
+        String str = "Tejashwa";
+        String str2= "Jasxxx";
+                String str3= "UVIX AI Developer";
+        System.out.println(str);System.out.println(str2);System.out.println(str3);
+    }
+}
