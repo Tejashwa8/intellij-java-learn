e5c4279dbba0050375199dd6325db0f93f56d465
Tejashwa Singh Sengar <tejassengar93@gmail.com>
2026-03-07 12:12:10 +0530

    init commit

---
+++ src/Main.java
@@ -0,0 +1,31 @@
+package com.company;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("The sum of the numbers is : ");
+        System.out.println("The mul of the numbers is : ");
+        int num1 = 4;
+        int num2 = 44;
+        int num3 = 444;
+        int num4 = 4;
+        int sum = num1 + num2 + num3 + num4;
+        System.out.println(sum);
+        int mul= num1 * num2 * num3 * num4;
+        System.out.println(mul);
+
+        System.out.println("The mul2 of the numbers is : ");
+        int num5 = 5;
+        int num6 = 40;
+        int num7 = 1;
+        int num8 = 6;
+        int mul2 = num5 * num6 * num7 * num8;
+        System.out.println(mul2);
+
+        System.out.println("The div of the numbers is : ");
+        int num11 = 50;
+        int num61 = 4;
+        int div = num11 / num61 ;
+        System.out.println(div);
+    }
+    }
+
